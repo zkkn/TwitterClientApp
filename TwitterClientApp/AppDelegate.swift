9e3b5d3ec81868349decf10aa0bac40c3f6d63a3
Shoichi Kanzaki <zaksho1121@gmail.com>
2017-06-17 16:24:49 +0900

    [add]ストーリーボードや既定のVCを削除して、Viewを初期化

--- TwitterClientApp/AppDelegate.swift
+++ TwitterClientApp/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let initialViewController = TweetTimelineViewController()
+        initialViewController.view.backgroundColor = .white
+        window?.rootViewController = initialViewController
+        
         return true
     }
 
