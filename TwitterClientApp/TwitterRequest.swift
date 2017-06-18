66f687646c5bed78316cb36e85470c598ab7554d
Shoichi Kanzaki <zaksho1121@gmail.com>
2017-06-18 19:37:23 +0900

    [add] added headerFields in TwitterAPI

---
+++ TwitterClientApp/TwitterRequest.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterRequest.swift
+//  TwitterClientApp
+//
+//  Created by Shoichi Kanzaki on 2017/06/18.
+//  Copyright © 2017年 mycompany. All rights reserved.
+//
+
+import APIKit
+import Foundation
+import Result
+
+var RequestCount = 0
+
+extension TwitterRequestType {
+    
+}
