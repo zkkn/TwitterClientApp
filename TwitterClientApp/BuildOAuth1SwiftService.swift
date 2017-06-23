207f68229c2fa35b3ed75c0e9287a76c6279047d
Shoichi Kanzaki <zaksho1121@gmail.com>
2017-06-23 15:11:09 +0900

    added BuildOAuth1Swift

---
+++ TwitterClientApp/BuildOAuth1SwiftService.swift
@@ -0,0 +1,20 @@
+//
+//  BuildOAuth1SwiftService.swift
+//  TwitterClientApp
+//
+//  Created by Shoichi Kanzaki on 2017/06/20.
+//  Copyright © 2017年 mycompany. All rights reserved.
+//
+
+import OAuthSwift
+
+class BuildOAuth1SwiftService {
+    
+    static var oauthswift = OAuth1Swift(
+        consumerKey: "Q8Zwi892fPBvAWLIxYP1YtlTQ",
+        consumerSecret: "uoUD6JLzxnvJG20vncmbZ13jYfgt4CQ50HZIVMENqCFYfQDGyc",
+        requestTokenUrl: "https://api.twitter.com/oauth/request_token",
+        authorizeUrl:    "https://api.twitter.com/oauth/authorize",
+        accessTokenUrl:  "https://api.twitter.com/oauth/access_token"
+    )
+}
