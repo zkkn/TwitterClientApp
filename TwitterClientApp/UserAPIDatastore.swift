df696a5a167e7e6e43306a8307c9bc75bee0f02b
Shoichi Kanzaki <zaksho1121@gmail.com>
2017-06-26 20:42:08 +0900

    removed arround GetUsers

--- TwitterClientApp/UserAPIDatastore.swift
+++ TwitterClientApp/UserAPIDatastore.swift
@@ -11,40 +11,10 @@ import Foundation
 import RxSwift
 
 protocol UserAPIDatastoreType {
-    func getUsers(userID: Int, screenName: Int, includeEntities: Bool) -> Observable<[String: Any]>
 }
 
 struct UserAPIDatastore: UserAPIDatastoreType {
-    
-    func getUsers(userID: Int, screenName: Int, includeEntities: Bool)
-        -> Observable<[String: Any]> {
-            return UserRequest
-                .GetUsers(userID: userID, screenName: screenName)
-                .send()
-    }
 }
 
 fileprivate struct UserRequest {
-    
-    fileprivate struct GetUsers: TwitterRequestType {
-        
-        let userID: Int
-        let screenName: Int
-        let includeEntities = false
-        
-        let method = HTTPMethod.get
-        let path = "users/show.json"
-        
-        var parameters: Any? {
-            let params: [String: Any] = [
-                "user_id": userID,
-                "screen_name": screenName,
-                "include_entities": includeEntities
-            ]
-            return params
-        }
-        func response(from object: Any, urlResponse: HTTPURLResponse) throws -> [String: Any] {
-            return object as! [String: Any]
-        }
-    }
 }
