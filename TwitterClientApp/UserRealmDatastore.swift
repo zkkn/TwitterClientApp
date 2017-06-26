bffae4392b7a63d26ad0fd64da2873e2121f94b0
Shoichi Kanzaki <zaksho1121@gmail.com>
2017-06-26 23:01:59 +0900

    modified after pull request

--- TwitterClientApp/UserRealmDatastore.swift
+++ TwitterClientApp/UserRealmDatastore.swift
@@ -17,7 +17,13 @@ struct UserRealmDatastore: RealmDatastore, UserDatabaseDatastoreType {
     typealias TargetObject = User
     
     func map(json: NSDictionary, to object: TargetObject, resetRelations: Bool) throws -> TargetObject {
+        let map = Mapper(JSON: json)
         
+        try object.twitterUserID = map.from("id")
+        try object.name = map.from("name")
+        try object.screenName = map.from("screen_name")
+        try object.profileImageURL = map.from("profile_image_url")
+        try object.profileImageURLHTTPS = map.from("profile_image_url_https")
         return object
     }
 }
