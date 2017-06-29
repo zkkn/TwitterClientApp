c36c06952bff634116b61ad166eb20c73e86ecd0
Shoichi Kanzaki <zaksho1121@gmail.com>
2017-06-29 10:24:29 +0900

    revised after pull request

---
+++ TwitterClientApp/RepositoryError.swift
@@ -0,0 +1,13 @@
+//
+//  RepositoryError.swift
+//  TwitterClientApp
+//
+//  Created by Shoichi Kanzaki on 2017/06/29.
+//  Copyright © 2017年 mycompany. All rights reserved.
+//
+
+import Foundation
+
+enum RepositoryError: Error {
+    case unknownError
+}
