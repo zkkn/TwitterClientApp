9b5712755ac9b064641e7ab12dd5d2c1475c11c4
Shoichi Kanzaki <zaksho1121@gmail.com>
2017-06-30 22:17:54 +0900

    implemented properties and initializer

--- TwitterClientApp/TimelineViewController.swift
+++ TwitterClientApp/TimelineViewController.swift
@@ -1,15 +1,16 @@
 //
-//  TweetTimelineViewController.swift
+//  TimelineViewController.swift
 //  TwitterClientApp
 //
 //  Created by Shoichi Kanzaki on 2017/06/17.
 //  Copyright © 2017年 mycompany. All rights reserved.
 //
 
+import RxSwift
 import SnapKit
 import UIKit
 
-final class TweetTimelineViewController: UIViewController {
+final class TimelineViewController: UIViewController {
     
     // MARK: - Views -
     
@@ -20,12 +21,31 @@ final class TweetTimelineViewController: UIViewController {
         tableView.rowHeight = UITableViewAutomaticDimension
         return tableView
     }()
+    
+    
+    // MARK - Properties -
+    
+    fileprivate let disposeBag = DisposeBag()
+    fileprivate let viewModel: TimelineViewModelType
+    
+    
+    // MARK - Initializer -
+    
+    init(viewModel: TimelineViewModelType) {
+        self.viewModel = viewModel
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 
 // MARK: - Life Cycle Events -
 
-extension TweetTimelineViewController {
+extension TimelineViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,7 +59,7 @@ extension TweetTimelineViewController {
 
 // MARK: - Setup -
 
-extension TweetTimelineViewController {
+extension TimelineViewController {
     
     fileprivate func configure() {
         view.backgroundColor = .white
