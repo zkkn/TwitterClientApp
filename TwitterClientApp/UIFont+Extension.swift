a42a4811d9ccd1cfe78d107b23d6d3a36520f512
Shoichi Kanzaki <zaksho1121@gmail.com>
2017-07-08 17:54:14 +0900

    updated TweetCell

--- TwitterClientApp/UIFont+Extension.swift
+++ TwitterClientApp/UIFont+Extension.swift
@@ -12,4 +12,8 @@ extension UIFont {
     class func hirakakuProNW3(size: CGFloat) -> UIFont? {
         return UIFont(name: "HirakakuProN-W3", size: size)
     }
+    
+    class func hirakakuProNW6(size: CGFloat) -> UIFont {
+        return UIFont(name: "HirakakuProN-W6", size: size)!
+    }
 }
