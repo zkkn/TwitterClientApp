243945f4c2f059236c8b91a7e7899da4f5b5f007
Shoichi Kanzaki <zaksho1121@gmail.com>
2017-07-08 12:19:49 +0900

    accesssed to tweet api home_timeline successfully

--- TwitterClientApp/LoginViewModel.swift
+++ TwitterClientApp/LoginViewModel.swift
@@ -66,11 +66,8 @@ final class LoginViewModel: LoginViewModelType, LoginViewModelInputs, LoginViewM
                         self?.oauthswift.client.get(
                             "https://api.twitter.com/1.1/statuses/home_timeline.json",
                             success: { response in
-                                let transform = "Any-Hex/Java"
-                                let responseString = response.string! as NSString
-                                let convertedString = responseString.mutableCopy() as! NSMutableString
-                                CFStringTransform(convertedString, nil, transform as NSString, true)
-                                print(convertedString)
+                                let jsonDict = try! response.jsonObject() as? [[String: Any]]
+                                print(jsonDict!)
                         },
                             failure: { error in
                                 print(error)
