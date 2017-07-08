fd94df339f1897a9103e7b0ed5e1bfbfed69f7e6
Shoichi Kanzaki <zaksho1121@gmail.com>
2017-07-08 17:54:14 +0900

    updated TweetCell

--- TwitterClientApp/TweetCell.swift
+++ TwitterClientApp/TweetCell.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class TweetCell: UITableViewCell {
-
+    
     // MARK: - Views -
+    
+    fileprivate lazy var profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    fileprivate lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.hirakakuProNW6(size: 15)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        label.textColor = .black
+        return label
+    }()
+    
+    fileprivate lazy var screenNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.hirakakuProNW3(size: 10)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        label.textColor = .lightGray
+        return label
+    }()
     
     fileprivate lazy var contentLabel: UILabel = {
         let contentLabel = UILabel()
-        contentLabel.font = UIFont.hirakakuProNW3(size: 10.0)
+        contentLabel.font = UIFont.hirakakuProNW3(size: 10)
         contentLabel.lineBreakMode = .byWordWrapping
         contentLabel.numberOfLines = 0
         contentLabel.textAlignment = .left
@@ -43,14 +68,33 @@ class TweetCell: UITableViewCell {
 extension TweetCell {
     
     fileprivate func setViews() {
+        addSubview(profileImageView)
+        addSubview(nameLabel)
+        addSubview(screenNameLabel)
         addSubview(contentLabel)
     }
     
     fileprivate func setConstraints() {
+        profileImageView.snp.makeConstraints { make in
+            make.left.top.equalTo(self).inset(8)
+            make.width.height.equalTo(50)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.left.equalTo(profileImageView.snp.right).offset(8)
+            make.top.right.equalTo(self).inset(8)
+        }
+        
+        screenNameLabel.snp.makeConstraints { make in
+            make.left.equalTo(profileImageView.snp.right).offset(8)
+            make.right.equalTo(self).inset(8)
+            make.top.equalTo(nameLabel.snp.bottom)
+        }
+        
         contentLabel.snp.makeConstraints { make in
-            make.left.right.equalTo(self).inset(20)
-            make.top.equalTo(self).inset(10)
-            make.bottom.equalTo(self).inset(30)
+            make.left.equalTo(profileImageView.snp.right).offset(8)
+            make.right.bottom.equalTo(self).inset(8)
+            make.top.equalTo(screenNameLabel.snp.bottom).offset(8)
         }
     }
 }
@@ -61,6 +105,20 @@ extension TweetCell {
 extension TweetCell {
     
     func update(tweet: Tweet) {
-        contentLabel.text = tweet.text
+        let downloadTask = URLSession.shared.dataTask(with: URL(string: (tweet.user?.profileImageURLHTTPS)!)!) {
+            [weak self] (data, response, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            DispatchQueue.main.async {
+                self?.profileImageView.image = UIImage(data: data!)
+            }
+        }
+        downloadTask.resume()
+        
+        self.screenNameLabel.text = "@" + (tweet.user?.screenName)!
+        self.nameLabel.text = tweet.user?.name
+        self.contentLabel.text = tweet.text
     }
 }
