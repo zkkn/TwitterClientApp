d2929bf5e8dba8f286e30f0bec07a27c9c10f36d
Shoichi Kanzaki <zaksho1121@gmail.com>
2017-07-10 17:30:41 +0900

    updated TimelineViewModel

--- TwitterClientApp/TimelineViewModel.swift
+++ TwitterClientApp/TimelineViewModel.swift
@@ -14,13 +14,20 @@ enum GetTweetResult {
     case failed
 }
 
+enum PostTweetResult {
+    case success
+    case failed
+}
+
 protocol TimelineViewModelInputs {
     var refreshRequest: PublishSubject<Void> { get }
+    var postTweet: PublishSubject<String> { get }
 }
 
 protocol TimelineViewModelOutputs {
     var tweets: Variable<[Tweet]> { get }
     var getTweetResult: PublishSubject<GetTweetResult> { get }
+    var postTweetResult: PublishSubject<PostTweetResult> { get }
 }
 
 protocol TimelineViewModelType {
@@ -40,12 +47,14 @@ final class TimelineViewModel: TimelineViewModelType, TimelineViewModelInputs, T
     // MARK - Inputs -
     
     let refreshRequest = PublishSubject<Void>()
+    let postTweet = PublishSubject<String>()
     
     
     // MARK: - Ouputs -
     
     let tweets = Variable<[Tweet]>([])
     let getTweetResult = PublishSubject<GetTweetResult>()
+    let postTweetResult = PublishSubject<PostTweetResult>()
     
     
     // MARK - Initializers -
@@ -76,6 +85,46 @@ final class TimelineViewModel: TimelineViewModelType, TimelineViewModelInputs, T
                         },
                         onError: { [weak self] (error) in
                             self?.getTweetResult.onNext(.failed)
+                    })
+                    .disposed(by: self!.disposeBag)
+            })
+            .disposed(by: disposeBag)
+        
+        postTweet
+            .subscribe(onNext: { [weak self] tweet in
+                guard let _ = self else { return }
+                TweetRepository(
+                    apiDatastore: TweetAPIDatastore(),
+                    tweetDBDatastore: TweetRealmDatastore(),
+                    selfInfoDBDatastore: SelfInfoDatabaseDatastore()
+                    )
+                    .postTweet(
+                        status: tweet
+                    )
+                    .subscribe(
+                        onNext: { [weak self] (_) in
+                            guard let _ = self else { return }
+                            TweetRepository(
+                                apiDatastore: TweetAPIDatastore(),
+                                tweetDBDatastore: TweetRealmDatastore(),
+                                selfInfoDBDatastore: SelfInfoDatabaseDatastore()
+                                )
+                                .getTweets(
+                                    requestNumberOfTweets: 100
+                                )
+                                .subscribe(
+                                    onNext: { [weak self] tweets in
+                                        self?.tweets.value = tweets
+                                        self?.getTweetResult.onNext(.success)
+                                    },
+                                    onError: { [weak self] (error) in
+                                        self?.getTweetResult.onNext(.failed)
+                                })
+                                .disposed(by: self!.disposeBag)
+                            self?.postTweetResult.onNext(.success)
+                        },
+                        onError: { [weak self] (error) in
+                            self?.postTweetResult.onNext(.failed)
                     })
                     .disposed(by: self!.disposeBag)
             })
